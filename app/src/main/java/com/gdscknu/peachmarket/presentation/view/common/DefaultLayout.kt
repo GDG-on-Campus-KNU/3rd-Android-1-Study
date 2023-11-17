42b34aa5917d7b0e14b389a9f9d2aa57bbbe6c0c
손홍석 <78216059+bayy1216@users.noreply.github.com>
2023-11-17 19:36:08 +0900

    [Feat]: DefaultLayout 파라미터 추가

--- app/src/main/java/com/gdscknu/peachmarket/presentation/view/common/DefaultLayout.kt
+++ app/src/main/java/com/gdscknu/peachmarket/presentation/view/common/DefaultLayout.kt
@@ -4,18 +4,28 @@ import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Notifications
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.gdscknu.peachmarket.presentation.theme.TextStyles.TITLE1_STYLE
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DefaultLayout(
     title: String? = null,
+    onSearchClick : () -> Unit = {},
+    onAlarmClick : () -> Unit = {},
+    floatingActionButton: @Composable () -> Unit = {},
     body: @Composable () -> Unit
 ){
     Scaffold (
@@ -23,11 +33,20 @@ fun DefaultLayout(
             title?.let {
                 TopAppBar(
                     title = {
-                        Text(text = title)
+                        Text(text = title, style = TITLE1_STYLE)
+                    },
+                    actions = {
+                        IconButton(onClick = onSearchClick) {
+                            Icon(Icons.Rounded.Search, contentDescription = null)
+                        }
+                        IconButton(onClick = onSearchClick) {
+                            Icon(Icons.Outlined.Notifications, contentDescription = null)
+                        }
                     }
                 )
             }
-        }
+        },
+        floatingActionButton = floatingActionButton,
     ){
         Box(modifier = Modifier.padding(it)) {
             body()
